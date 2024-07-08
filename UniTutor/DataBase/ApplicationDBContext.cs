26ee4ef70818ec1f185c88bbb15afe522e1b9d53
Abivarsan <ketheesabi7400@gmail.com>
2024-07-08 15:36:50 +0530

    8/7 3.36PM

--- UniTutor/DataBase/ApplicationDBContext.cs
+++ UniTutor/DataBase/ApplicationDBContext.cs
@@ -3,7 +3,7 @@ using UniTutor.Model;
 namespace UniTutor.DataBase;
 
 
-public class ApplicationDBContext:DbContext
+public class ApplicationDBContext : DbContext
 {
     public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
     {
@@ -13,20 +13,41 @@ public class ApplicationDBContext:DbContext
     public DbSet<Student> Students { get; set; }
     public DbSet<Tutor> Tutors { get; set; }
     public DbSet<Subject> Subjects { get; set; }
-   // public DbSet<Review> Reviews { get; set; }
+    // public DbSet<Review> Reviews { get; set; }
     public DbSet<Request> Requests { get; set; }
     public DbSet<Comment> Comments { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<Student>()
-                .HasMany(s => s.Comments)
-                .WithOne(c => c.Student)
-                .HasForeignKey(c => c.stuId);
+        
+            
 
         modelBuilder.Entity<Tutor>()
             .HasMany(t => t.Comments)
             .WithOne(c => c.Tutor)
-            .HasForeignKey(c => c.tutId);
+            .HasForeignKey(c => c.tutId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        // Configure the Comment entity
+        modelBuilder.Entity<Comment>()
+            .Property(c => c.userType)
+            .IsRequired();
+
+        modelBuilder.Entity<Comment>()
+            .Property(c => c.commentText)
+            .IsRequired();
+
+        modelBuilder.Entity<Comment>()
+            .Property(c => c.time)
+            .IsRequired();
+
+        modelBuilder.Entity<Request>()
+                       .HasOne(sr => sr.Student)
+                       .WithMany(s => s.Requests)
+                       .HasForeignKey(sr => sr.studentId)
+                       .OnDelete(DeleteBehavior.Restrict);
+        
+
+       
 
         modelBuilder.Entity<Tutor>()
                .Property(t => t.CreatedAt)
@@ -37,7 +58,24 @@ public class ApplicationDBContext:DbContext
                .HasColumnType("datetime2");
 
         base.OnModelCreating(modelBuilder);
+
+
+
+        modelBuilder.Entity<Request>()
+            .HasOne(sr => sr.Subject)
+            .WithMany(s => s.Requests)
+            .HasForeignKey(sr => sr.subjectId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        modelBuilder.Entity<Request>()
+            .HasOne(sr => sr.Tutor)
+            .WithMany(t => t.Requests)
+            .HasForeignKey(sr => sr.tutorId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        base.OnModelCreating(modelBuilder);
     }
-
-
 }
+
+
+
